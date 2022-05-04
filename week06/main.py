c635ee4cd0e5ea45a31bb2f033d35daef4a94307
mindlove4 <60215505+mindlove4@users.noreply.github.com>
2022-05-05 04:43:04 +0700

    change host

--- week06/main.py
+++ week06/main.py
@@ -7,4 +7,4 @@ def hello_world():
     return 'Hello from host'
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=8000)
+    app.run(debug=True,host='20.216.26.23',port=8000)
